7478d3c5c58f1f5594a2a8dc8dc31293c96412a7
KC21077 <ychen32@uw.edu>
2023-05-10 20:49:32 -0700

    hw8

--- Import.py
+++ Import.py
@@ -2,6 +2,8 @@ import sqlite3
 import sys
 import xml.etree.ElementTree as ET
 
+connection = sqlite3.connect('pokemon.sqlite')
+cursor = connection.cursor()
 # Incoming Pokemon MUST be in this format
 #
 # <pokemon pokedex="" classification="" generation="">
@@ -26,10 +28,48 @@ import xml.etree.ElementTree as ET
 # Read pokemon XML file name from command-line
 # (Currently this code does nothing; your job is to fix that!)
 if len(sys.argv) < 2:
-    print("You must pass at least one XML file name containing Pokemon to insert")
+    print("Usage: python Import.py [pokedex.xml]")
 
 for i, arg in enumerate(sys.argv):
     # Skip if this is the Python filename (argv[0])
     if i == 0:
         continue
 
+tree = ET.parse('pokedex.xml')
+root = tree.getroot()
+
+#Extract data from pokemon xml and put it into the database
+for pokemon in root:
+    pokedex = pokemon.get('pokedexNumber')
+    classification = pokemon.get('classification')
+    generation = pokemon.get('generation')
+    name = pokemon.find('name').text
+    hp = pokemon.find('hp').text
+    types = [t.text for t in pokemon.findall('type')]
+    attack = pokemon.find('attack').text
+    defense = pokemon.find('defense').text
+    speed = pokemon.find('speed').text
+    sp_attack = pokemon.find('sp_attack').text
+    sp_defense = pokemon.find('sp_defense').text
+    height = pokemon.find('height/m').text
+    weight = pokemon.find('weight/kg').text
+    abilities = [a.text for a in pokemon.findall('abilities/ability')]
+
+    cursor.execute('SELECT name FROM Pokemon WHERE name = ?', (name,))
+    row = cursor.fetchone()
+    if row is not None:
+        print(f'{name} is already in the database')
+        continue
+
+cursor.execute('''INSERT INTO imported_pokemon_data (pokedex_number, classfication, generation, name, hp, type1, type2, attack, defense, speed, sp_attack, sp_defense, height_m, weight_kg)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+                (pokedex, classification, generation, name, hp, types[0], types[1] if len(types) > 1 else None, attack, defense, speed, sp_attack, sp_defense, height, weight))
+pokemon_id = cursor.lastrowid
+
+for ability in abilities:
+    cursor.execute('INSERT INTO imported_pokemon_data (pokedex_number, abilities) VALUES (?, ?)', (pokemon_id, ability))
+
+print(f'{name} inserted into the database')
+
+connection.commit()
+connection.close()
--- genone.xsl
+++ genone.xsl
@@ -14,13 +14,14 @@
   in order to pull back the correct XML nodes. In some of the other stylesheets, you will
   need to write the full "apply-templates" text yourself.
   -->
-<xsl:template match="/pokedex">
-    <xsl:apply-templates select="XPATH-QUERY-GOES-HERE" />
-</xsl:template>
 
-<xsl:template match="pokemon">
-    <xsl:value-of select="XPATH-QUERY-GOES-HERE" /> (<xsl:value-of select="XPATH-QUERY-GOES-HERE" />): <xsl:value-of select="XPATH-QUERY-GOES-HERE" /> | 
-</xsl:template>
+  <xsl:template match="/pokedex">
+      <xsl:apply-templates select= "pokemon[@generation = '1']"/>
+  </xsl:template>
+
+  <xsl:template match="pokemon">
+      <xsl:value-of select="name" /> (<xsl:value-of select="@pokedexNumber" />): <xsl:value-of select="@classification"/> |
+  </xsl:template>
 
 <!--
   These rules will generate HTML output rather than text. This is to demonstrate
@@ -31,7 +32,8 @@
 
   ... where the name and the parenthesized Pokedex number are in the first column.
   -->
-<!--
+  <!--
+
 <xsl:template match="/pokedex">
   <html>
   <body>
@@ -41,7 +43,7 @@
       <th>Name (Pokedex Number)</th>
       <th>Classification</th>
     </tr>
-    <xsl:apply-templates select="XPATH-QUERY-GOES-HERE" />
+    <xsl:apply-templates select="pokemon[@generation = '1']" />
   </table>
   </body>
   </html>
@@ -49,10 +51,11 @@
 
 <xsl:template match="pokemon">
     <tr>
-      <td><xsl:value-of select="XPATH-QUERY-GOES-HERE" />(<xsl:value-of select="XPATH-QUERY-GOES-HERE" />)</td>
-      <td><xsl:value-of select="XPATH-QUERY-GOES-HERE" /></td>
+      <td><xsl:value-of select="./name" />(<xsl:value-of select="＠pokedexNumber" />)</td>
+      <td><xsl:value-of select= "./@classification" /></td>
     </tr>
 </xsl:template>
 -->
+
 
 </xsl:stylesheet>
